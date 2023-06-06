c6fe56696b5a31c5ec3457358f1579957eed0e74
asavoullis <cs16538@bristol.ac.uk>
2023-06-06 09:44:14 +0100

    added more examples

--- CodingBat.py
+++ CodingBat.py
@@ -13,10 +13,8 @@ def sleep_in(weekday, vacation):
 
 
 # Warmup-1 > monkey_trouble
-#
 # We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
 # We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
-#
 # monkey_trouble(True, True) → True
 # monkey_trouble(False, False) → True
 # monkey_trouble(True, False) → False
@@ -30,7 +28,6 @@ def monkey_trouble(a_smile, b_smile):
 # Warmup-1 > sum_double
 # Given two int values, return their sum.
 # Unless the two values are the same, then return double their sum.
-#
 # sum_double(1, 2) → 3
 # sum_double(3, 2) → 5
 # sum_double(2, 2) → 8
@@ -44,7 +41,6 @@ def sum_double(a, b):
 # Warmup-1 > diff21
 # Given an int n, return the absolute difference between n and 21,
 # except return double the absolute difference if n is over 21.
-#
 # diff21(19) → 2
 # diff21(10) → 11
 # diff21(21) → 0
@@ -58,7 +54,6 @@ def diff21(n):
 # Warmup-1 > parrot_trouble
 # We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
 # We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
-#
 # parrot_trouble(True, 6) → True
 # parrot_trouble(True, 7) → False
 # parrot_trouble(False, 6) → False
@@ -71,7 +66,6 @@ def parrot_trouble(talking, hour):
 
 # Warmup-1 > makes10
 # Given 2 ints, a and b, return True if one of them is 10 or if their sum is 10.
-#
 # makes10(9, 10) → True
 # makes10(9, 9) → False
 # makes10(1, 9) → True
@@ -85,7 +79,6 @@ def makes10(a, b):
 
 # Warmup-1 > near_hundred
 # Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
-#
 # near_hundred(93) → True
 # near_hundred(90) → True
 # near_hundred(89) → False
@@ -100,7 +93,6 @@ def near_hundred(n):
 # Warmup-1 > not_string
 # Given a string, return a new string where "not " has been added to the front.
 # However, if the string already begins with "not", return the string unchanged.
-#
 # not_string('candy') → 'not candy'
 # not_string('x') → 'not x'
 # not_string('not bad') → 'not bad'
@@ -114,7 +106,6 @@ def not_string(str):
 # Given a non-empty string and an int n, return a new string where the char at index n has been removed.
 # The value of n will be a valid index of a char in the original string
 # (i.e. n will be in the range 0..len(str)-1 inclusive).
-#
 # missing_char('kitten', 1) → 'ktten'
 # missing_char('kitten', 0) → 'itten'
 # missing_char('kitten', 4) → 'kittn'
@@ -124,7 +115,6 @@ def missing_char(str, n):
 
 # Warmup-1 > front_back
 # Given a string, return a new string where the first and last chars have been exchanged.
-#
 # front_back('code') → 'eodc'
 # front_back('a') → 'a'
 # front_back('ab') → 'ba'
@@ -139,7 +129,6 @@ def front_back(str):
 # Given a string, we'll say that the front is the first 3 chars of the string.
 # If the string length is less than 3, the front is whatever is there.
 # Return a new string which is 3 copies of the front.
-#
 # front3('Java') → 'JavJavJav'
 # front3('Chocolate') → 'ChoChoCho'
 # front3('abc') → 'abcabcabc'
@@ -151,7 +140,6 @@ def front3(str):
 
 # Warmup-2 > string_times
 # Given a string and a non-negative int n, return a larger string that is n copies of the original string.
-#
 # string_times('Hi', 2) → 'HiHi'
 # string_times('Hi', 3) → 'HiHiHi'
 # string_times('Hi', 1) → 'Hi'
@@ -166,7 +154,6 @@ def string_times2(str, n):
 # Warmup-2 > front_times
 # Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
 # or whatever is there if the string is less than length 3. Return n copies of the front;
-#
 # front_times('Chocolate', 2) → 'ChoCho'
 # front_times('Chocolate', 3) → 'ChoChoCho'
 # front_times('Abc', 3) → 'AbcAbcAbc'
@@ -183,12 +170,78 @@ def front_times2(str, n):
     return result
 
 
-Warmup-2 > string_bits
-Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+# Warmup-2 > string_bits
+# Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+# string_bits('Hello') → 'Hlo'
+# string_bits('Hi') → 'H'
+# string_bits('Heeololeo') → 'Hello'
+def string_bits(str):
+    return str[0:len(str):2]
 
 
-string_bits('Hello') → 'Hlo'
-string_bits('Hi') → 'H'
-string_bits('Heeololeo') → 'Hello'
+# Warmup-2 > string_splosion
+# Given a non-empty string like "Code" return a string like "CCoCodCode".
+# string_splosion('Code') → 'CCoCodCode'
+# string_splosion('abc') → 'aababc'
+# string_splosion('ab') → 'aab'
+def string_splosion(str):
+    result = ""
+    for i in range(len(str)):
+        result += str[:i+1]
+    return result
+
+
+# Warmup-2 > last2     HARD
+# Given a string, return the count of the number of times that a substring length 2 appears in the string
+# and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+# last2('hixxhi') → 1
+# last2('xaxxaxaxx') → 1
+# last2('axxxaaxx') → 2
+def last2(input_string):
+    count = 0
+    last_two = input_string[-2:]
+
+    for i in range(len(input_string) - 2):
+        if input_string[i:i + 2] == last_two:
+            count += 1
+
+    return count
+
+
+# Warmup-2 > array_count9
+# Given an array of ints, return the number of 9's in the array.
+# array_count9([1, 2, 9]) → 1
+# array_count9([1, 9, 9]) → 2
+# array_count9([1, 9, 9, 3, 9]) → 3
+def array_count9(nums):
+    count = 0
+    for num in nums:
+        if num == 9:
+            count += 1
+    return count
+
+
+# Warmup-2 > array_front9
+# Given an array of ints, return True if one of the first 4 elements in the array is a 9.
+# The array length may be less than 4.
+# array_front9([1, 2, 9, 3, 4]) → True
+# array_front9([1, 2, 3, 4, 9]) → False
+# array_front9([1, 2, 3, 4, 5]) → False
+def array_front9(nums):
+    return any(num == 9 for num in nums[:4])
+
+def array_front9_v2(nums):
+    for i in range(len(nums)):
+        if nums[i] == 9 and i < 4:
+            return True
+    return False
+
+
+
+
+
+
+
+
 
 
