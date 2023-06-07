aaedef7e10b36e87f88fa7bfbb121daa89c2eefc
asavoullis <cs16538@bristol.ac.uk>
2023-06-07 09:57:01 +0100

    more examples added

--- CodingBat.py
+++ CodingBat.py
@@ -274,5 +274,56 @@ def string_match(a, b):
     return count
 
 
+# String-1 > hello_name
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+# hello_name('Bob') → 'Hello Bob!'
+# hello_name('Alice') → 'Hello Alice!'
+# hello_name('X') → 'Hello X!'
+def hello_name(name):
+    return "Hello " + name + "!"
+
+
+# String-1 > make_abba
+# Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+# make_abba('Hi', 'Bye') → 'HiByeByeHi'
+# make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+# make_abba('What', 'Up') → 'WhatUpUpWhat'
+def make_abba(a, b):
+    return a + b + b + a
+
+
+# String-1 > make_tags
+# The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text.
+# In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+# Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+# make_tags('i', 'Yay') → '<i>Yay</i>'
+# make_tags('i', 'Hello') → '<i>Hello</i>'
+# make_tags('cite', 'Yay') → '<cite>Yay</cite>'
+def make_tags(tag, word):
+    return "<" + tag + ">" + word + "</" + tag + ">"
+
+
+# String-1 > make_out_word
+# Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+# make_out_word('<<>>', 'Yay') → '<<Yay>>'
+# make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+# make_out_word('[[]]', 'word') → '[[word]]'
+def make_out_word(out, word):
+    return out[:len(out)/2] + word + out[len(out)/2:]
+
+
+# String-1 > extra_end
+# Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+# extra_end('Hello') → 'lololo'
+# extra_end('ab') → 'ababab'
+# extra_end('Hi') → 'HiHiHi'
+def extra_end(str):
+    return 3*str[-2:]
+
+
+
+
+
+
 
 
